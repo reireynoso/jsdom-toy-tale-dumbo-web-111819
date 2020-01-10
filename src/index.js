d7435d1335c23444188d93f4d1d17b58dc5a64a5
reireynoso <reireynoso@gmail.com>
2020-01-09 22:01:33 -0500

    added general approach comment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 let addToy = false
 
+// General Approach 
+// 1. Fetch data from a server
+// 2. Generate elements for the data
+// 3. Slap that on the DOM
+// 4. Add event listeners to make the page interactive
+// 5. Make changes that persist on the server without having to refresh the page
+
 document.addEventListener("DOMContentLoaded", ()=>{
   const addBtn = document.querySelector('#new-toy-btn')
   const toyForm = document.querySelector('.container')
